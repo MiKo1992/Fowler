16d628867956c77edae3ca23a363a0ac54b99c29
Michael Kolb <mikokolb@yahoo.de>
2016-05-02 09:21:16 +0200

    Tests erstellt

--- test/CustomerTest.java
+++ test/CustomerTest.java
@@ -8,7 +8,20 @@ import static org.junit.Assert.*;
 public class CustomerTest {
     @Test
     public void statement() throws Exception {
-
+        Movie m1 = new Movie("movie1", 1);
+        Movie m2 = new Movie("movie2", 2);
+        Rental r1 = new Rental(m1, 10);
+        Rental r2 = new Rental(m2, 5);
+        Customer c1 = new Customer("joe");
+        c1.addRental(r1);   c1.addRental(r2);
+        String zurueckGegebenesStatement= c1.statement();
+        String erwartetesStatement = "Rental Record for joe\n" +
+                "\tTitle\t\tDays\tAmount\n" +
+                "\tmovie1\t\t10\t30.0\n" +
+                "\tmovie2\t\t5\t4.5\n" +
+                "Amount owed is 34.5\n" +
+                "You earned 3 frequent renter points";
+        assertEquals("String Statement stimmt NICHT ueberein", erwartetesStatement, zurueckGegebenesStatement);
     }
 
 }
