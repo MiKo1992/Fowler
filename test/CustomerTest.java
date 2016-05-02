8c85c06dec8de4fab1196539c665688d63e68daf
Michael Kolb <mikokolb@yahoo.de>
2016-05-02 09:10:21 +0200

    Testclass Customer erstellt

---
+++ test/CustomerTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mikok on 02.05.2016.
+ */
+public class CustomerTest {
+    @Test
+    public void statement() throws Exception {
+
+    }
+
+}
