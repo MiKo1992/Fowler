346efc4231aad6ad17c747c647bc91879231669b
Michael Kolb <mikokolb@yahoo.de>
2016-05-02 17:38:05 +0200

    move Method, Extract Method, Polymorphism

--- src/NewReleasePrice.java
+++ src/NewReleasePrice.java
@@ -1,8 +1,18 @@
 /**
  * Created by mikok on 02.05.2016.
  */
-public class NewReleasePrice extends Price {
-    int getPriceCode(){
-        return Movie.NEW_RELEASE;
+ class NewReleasePrice extends Price {
+        int getPriceCode() {
+                 return Movie.NEW_RELEASE;
+             }
+
+
+                 double getCharge(int daysRented){
+                 return daysRented * 3;
+             }
+
+
+                 int getFrequentRenterPoints(int daysRented) {
+                 return (daysRented > 1) ? 2: 1;
     }
 }
